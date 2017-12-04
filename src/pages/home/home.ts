a6ecd7c33689c99338d3b7cf539fc10bee735e4b
Tsvetomir Ivanov <tsvetomir.i.wiredelta@gmail.com>
2017-12-04 22:33:25 +0200

    Initial integration of google maps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapRef: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('test');
+    console.log(this.mapRef);
+    this.initMap();
+  }
+
+
+  initMap() {
+    // Location - lat, long
+    const mapInitLocation = new google.maps.LatLng(51.11, 21.11);
+
+    // Map options
+    const mapOptions = {
+      center: mapInitLocation,
+      zoom: 17
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, mapOptions);
 
   }
 
