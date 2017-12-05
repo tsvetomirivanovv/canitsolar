7bad6e9c45ef9558bac7ac2ee14302bf5eca1c20
Tsvetomir Ivanov <tsvetomir.i.wiredelta@gmail.com>
2017-12-05 04:08:05 +0200

    Add marker with Latitude and longitude on map click

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,31 +10,50 @@ declare var google: any;
 export class HomePage {
 
   @ViewChild('map') mapRef: ElementRef;
-  map: any;
 
   constructor(public navCtrl: NavController) {
 
   }
 
   ionViewDidLoad() {
-    console.log('test');
-    console.log(this.mapRef);
     this.initMap();
   }
 
-
+  // Initialization of the google map
   initMap() {
+
     // Location - lat, long
-    const mapInitLocation = new google.maps.LatLng(51.11, 21.11);
+    const mapInitLocation = new google.maps.LatLng(52.11, 22.11);
 
     // Map options
     const mapOptions = {
       center: mapInitLocation,
-      zoom: 17
+      zoom: 13
     };
 
-    this.map = new google.maps.Map(this.mapRef.nativeElement, mapOptions);
+    // Creating a map with the provided map options
+    const map = new google.maps.Map(this.mapRef.nativeElement, mapOptions);
 
+    // Add new marker on map left click
+    this.addMapMarker(map);
+
+  }
+
+
+  addMapMarker(map) {
+    // Create new empty marker
+    let mapMarker1 = new google.maps.Marker;
+    // Map event for click
+    map.addListener('click', function (event) {
+      // Delete the old marker
+      mapMarker1.setMap(null);
+      mapMarker1 = null;
+      // Create new marker
+      mapMarker1 = new google.maps.Marker({
+        position: new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()),
+        map: map
+      });
+    });
   }
 
 }
